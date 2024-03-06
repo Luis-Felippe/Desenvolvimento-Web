ffe1adf28187163e116a04a08e58b489283c1da9
Luis Felippe <luisfelippem@gmail.com>
2024-03-05 22:43:44 -0300

    organizando algumas coisas

--- atividade001/atividade001/src/App.js
+++ atividade001/atividade001/src/App.js
@@ -1,19 +1,13 @@
-//import Meusdados2 from "./components/02Meusdados";
+import Meusdados2 from "./components/02Meusdados";
 import Temperatura from "./components/03Temperatura";
 
-/*
 function App() {
   return (
     <div className="App">
       <Meusdados2 nome = "Luis Felippe Morais de lima" curso = "Engenharia de Software" universidade = "Universidade Federal do Ceará" />
-    </div>
-    
-  );
-}
-*/
-function App() {
-  return (
-    <div className="App">
+      <h1>
+        ____________________________________________________
+      </h1>
       <Temperatura grau = "32" />
     </div>
     
